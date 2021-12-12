b1e656e2b3252952725dd266375b503b906cc305
FranciscoCoya <kikocoya@gmail.com>
2021-12-12 13:54:12 +0100

    Añadiendo asignaturas y mostrando listado en /asignaturas - v1.00

--- app/routes/asignaturas/index.jsx
+++ app/routes/asignaturas/index.jsx
@@ -1,7 +1,35 @@
+import { useLoaderData } from "remix";
+
+import * as data from "./asignaturas.json";
+
+export const loader = () => {
+  return [
+    {
+      slug: "SI",
+      denominacion: "Sistemas informáticos",
+    },
+    {
+      slug: "BBDD",
+      denominacion: "Bases de datos",
+    }
+  ]
+
+};
+
 export default function Asignaturas() {
+  const asignaturas = useLoaderData();
   return (
     <div className="">
       <h1>Asignaturas DAW</h1>
+      <ul>
+        {
+          asignaturas.map(a => (
+            <li key={a.slug}>
+              {a.denominacion}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 };
